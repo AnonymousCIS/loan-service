4d7a626196a8eec874cf2ffab3082a829673d039
kks940104 <kks940104@gmail.com>
2025-02-07 00:32:50 +0900

    readme 추가

--- src/main/java/org/anonymous/global/configs/SecurityConfig.java
+++ src/main/java/org/anonymous/global/configs/SecurityConfig.java
@@ -66,7 +66,9 @@ public class SecurityConfig {
 
                     // 관리자 접근 가능 패턴
                    c.requestMatchers("/admin/**").hasAnyAuthority("ADMIN")
-
+                           .requestMatchers("/apidocs.html",
+                                   "/swagger-ui/**",
+                                   "/api-docs/**").permitAll()
                            // 나머지는 아무 인증, 로그인만 접근 가능
                            .anyRequest().authenticated();
                 });
